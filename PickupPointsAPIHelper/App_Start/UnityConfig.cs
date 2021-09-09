a96d89562734ebe2395e1341b503fcd010b398f8
Chris <Chrisminoko@gmail.com>
2021-09-09 21:52:52 +0200

    jkhj

--- PickupPointsAPIHelper/App_Start/UnityConfig.cs
+++ PickupPointsAPIHelper/App_Start/UnityConfig.cs
@@ -12,7 +12,7 @@ namespace PickupPointsAPIHelper
             
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+            //jkkj
             // e.g. container.RegisterType<ITestService, TestService>();
             
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
